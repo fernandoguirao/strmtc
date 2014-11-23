f6789e6940fc4eebebc63e95bda94ba3a5cdeaf3
Fernando Guirao <fernando@bueninvento.es>
2014-11-23 09:23:09 +0100

    Antes de volver a sustituir jade por php

---
+++ wp-content/themes/lovster/includes/foot.php
@@ -0,0 +1,11 @@
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo $templatePath; ?>assets/js/jquery.min.js"><\/script>')</script>
+<script src="<?php echo $templatePath; ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?php echo $templatePath; ?>assets/js/app.js"></script>
+<script src="<?php echo $templatePath; ?>assets/js/retina.min.js"></script>
+<script>
+  //<![CDATA[
+  document.write("<script async src='//HOST:3000/browser-sync-client.1.3.7.js'><\/script>".replace(/HOST/g, location.hostname));
+  //]]>
+</script>
